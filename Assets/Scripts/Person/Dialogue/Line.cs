98b7a09fa93e979004179ffa69e28aa93efff2db
Millmoss <omelliff@yahoo.com>
2018-12-05 20:13:06 -0500

    Dialogue is a nightmare halp

--- Assets/Scripts/Person/Dialogue/Line.cs
+++ Assets/Scripts/Person/Dialogue/Line.cs
@@ -6,9 +6,16 @@ public class Line
 {
 	public string line { get; private set; }
 	private Enums.descriptors[] keys;
+	private string[] other;
 	private Noun[] nouns;
 	private Verb[] verbs;
 	private Adjective[] adjectives;
 
+	public Line(string l)
+	{
+		for (int i = 0; i < l.Length; i++)
+		{
 
+		}
+	}
 }
---
+++ Assets/Scripts/Person/Dialogue/LineLibrary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LineLibrary
+{
+	public string[] opinionDirected;
+	public string[] opinionUndirected;
+	public string[] requestDirected;
+	public string[] insultDirected;
+	public string[] threatDirected;
+	public string[] opinionOpinionSimple;
+	public string[] questionReason;
+	public string[] answerRequest;
+	public string[] answerDanger;
+
+	public static LineLibrary CreateFromJSON(string jsonString)
+	{
+		return JsonUtility.FromJson<LineLibrary>(jsonString);
+	}
+
+	public Line getLine(string type)
+	{
+
+	}
+}
