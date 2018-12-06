4711256bbc0df74ed45f0525c185c4d444606bfb
Millmoss <omelliff@yahoo.com>
2018-12-06 00:23:27 -0500

    Conversation.cs is almost done for now kind of.

--- Assets/Scripts/Person/Dialogue/Conversation.cs
+++ Assets/Scripts/Person/Dialogue/Conversation.cs
@@ -4,35 +4,141 @@ using UnityEngine;
 
 public class Conversation
 {
+	private LineLibrary ll;
+	private ExpressionLibrary el;
 
+	public Conversation()
+	{
+		ll = FileManager.initLines();   //this will be creating a new instance for each character if conversation class isn't a universal class
+		el = FileManager.initExpressions();
+	}
 
-	public void speak(Person from, Person to, Person person, float feeling) //speaks of person
+	public void speak(Person from, Person to, Person person, float feeling, int lower) //speaks of person
+	{
+		Enums.lineTypes type = determineLine(from, to, person, null, "person", feeling);
+		List<string> lineStrings = ll.getLineString(type);
+
+		for (int i = 0; i < lineStrings.Count; i++)
+		{
+			if (lineStrings[i][0] == '.' && lineStrings[i].Length > 1)
+			{
+				Expression e = determineExpression(from, to, person, feeling, lineStrings[i], type, lower);
+			}
+		}
+	}
+
+	public void speak(Person from, Person to, Thing thing, float feeling, int lower)    //speaks of thing
 	{
 		int i = 0;
 	}
 
-	public void speak(Person from, Person to, Person person, string feature, float feeling) //speaks of feature of person
+	public void speak(Person from, Person to, Line line, float feeling, int lower)    //speaks of line, this is a response
 	{
 		int i = 0;
 	}
 
-	public void speak(Person from, Person to, Person person, Thing thing, float feeling)    //speaks of thing of person
+	public Enums.lineTypes determineLine(Person from, Person to, Person person, Thing thing, string about, float feeling)
 	{
-		int i = 0;
+		Enums.lineTypes type = Enums.lineTypes.answerDanger;
+
+		if (feeling < 0)	//this prevents people with high chill from threatening people cause that's not chill at all
+			feeling += from.chill / 2;
+
+		if (about == "person")
+		{
+			if (to.name.Equals(person.name))
+			{
+				if (feeling < -.8f)
+					type = Enums.lineTypes.threatDirected;
+				else if (feeling < -.45f)
+					type = Enums.lineTypes.insultDirected;
+				else
+					type = Enums.lineTypes.opinionDirected;
+			}
+			else
+			{
+				type = Enums.lineTypes.opinionUndirected;
+			}
+		}
+		else if (about == "line")
+		{
+			if (feeling < -.7f)
+				type = Enums.lineTypes.threatDirected;
+			else if (feeling < -.35f)
+				type = Enums.lineTypes.insultDirected;
+			else if (feeling < .4f)
+				type = Enums.lineTypes.opinionOpinionSimple;
+		}
+
+		return type;
 	}
 
-	public void speak(Person from, Person to, Person person, Thing thing, string feature, float feeling)        //speaks of feature of thing of person
+	public Expression determineExpression(Person from, Person to, Person person, float feeling, string tempExpression, Enums.lineTypes type, int lowerBound)
 	{
-		int i = 0;
-	}
+		Expression e = new Expression();
 
-	public void speak(Person from, Person to, Thing thing, float feeling)    //speaks of thing
-	{
-		int i = 0;
-	}
+		switch (tempExpression)
+		{
+				case ".feelingVerb":
+				{
+					break;
+				}
+				case ".amountAdverb":
+				{
+					break;
+				}
+				case ".feelingAdjective":
+				{
+					break;
+				}
+				case ".person.feature":
+				{
+					break;
+				}
+				case ".noun":
+				{
+					if (type == Enums.lineTypes.insultDirected)
+					{
+						string near = "rubber duck";//from.getRandomNearbyItem();
+						Noun n = new Noun(near, Enums.generalTypes.thing, "none");
+						e = n;
+					}
+					else
+					{
+						Noun n = new Noun(person.name, Enums.generalTypes.person, person.name);
+						e = n;
+					}
+					break;
+				}
+				case ".noun.feature":
+				{
+					string f = "butt";//person.getMatchingFeature(feeling);
+					Noun n = new Noun(f, Enums.generalTypes.feature, person.name);
+					e = n;
+					break;
+				}
+				case ".agreementVerb":
+				{
+					Noun n = new Noun("line", Enums.generalTypes.line, to.name);
+					Enums.descriptors d;
+					if (feeling > .33f)
+						d = Enums.descriptors.agreeable;
+					else if (feeling < -.33f)
+						d = Enums.descriptors.disagreeable;
+					else
+						d = Enums.descriptors.safe;
+					Enums.descriptors[] desc = new Enums.descriptors[1];
+					desc[0] = d;
+					Verb v = new Verb(el.getExpressionString(Enums.expressionTypes.agreementVerb, feeling, lowerBound), desc, n);
+					e = v;
+					break;
+				}
+				default:
+				{
+					break;
+				}
+		}
 
-	public void speak(Person from, Person to, Thing thing, string feature, float feeling)        //speaks of feature of thing
-	{
-		int i = 0;
+		return e;
 	}
 }
--- Assets/Scripts/Person/Dialogue/Expressions/Noun.cs
+++ Assets/Scripts/Person/Dialogue/Expressions/Noun.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Noun : Expression
 {
-	public string type { get; private set; }	//feature, person, thing, etc
+	public Enums.generalTypes type { get; private set; }	//feature, person, thing, etc
 	public string owner { get; private set; }   //if it is a feature, this would be person or thing, if it is a thing held by person, owner would be person, and so on
 
-	public Noun(string noun, string typ, string ownr)
+	public Noun(string noun, Enums.generalTypes typ, string ownr)
 	{
 		expression = noun;
 		type = typ;
